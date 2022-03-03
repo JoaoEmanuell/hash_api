cbd20a37781041ac864d52b21fea84e7e9665642
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-03-03 18:23:05 -0300

    api, compare post and adjuste generate post

--- routers/api/api_route.py
+++ routers/api/api_route.py
@@ -1,5 +1,7 @@
 # Global imports
 from flask import Blueprint, jsonify, request
+from json import loads
+from json.decoder import JSONDecodeError
 
 # Local Imports
 from .source import Hash
@@ -12,12 +14,26 @@ def generate() -> dict :
         return jsonify({'hash' : Hash().generate_hash(request.args.get('value'))})
     elif request.method == 'POST':
         try : 
-            return jsonify({'hash' : Hash().generate_hash(request.form['value'])})
+            data = loads(request.data)
+            return jsonify({'hash' : Hash().generate_hash(data['value'])})
         except KeyError:
             return jsonify({'hash' : 'Invalid Key'})
+        except JSONDecodeError :
+            return jsonify({'hash' : 'Invalid JSON'})
     else :
         return 'Method not allowed'
 
-@api.route('/compare/')
+@api.route('/compare/', methods=['GET', 'POST'])
 def compare() -> dict :
-    return jsonify({'status' : Hash().compare_hash(request.args.get('value'), request.args.get('hash'))})
+    if request.method == 'GET':
+        return jsonify({'status' : Hash().compare_hash(request.args.get('value'), request.args.get('hash'))})
+    elif request.method == 'POST':
+        try : 
+            data = loads(request.data)
+            return jsonify({'status' : Hash().compare_hash(data['value'], data['hash'])})
+        except KeyError :
+            return jsonify({'status' : 'Invalid Key'})
+        except JSONDecodeError :
+            return jsonify({'status' : 'Invalid JSON'})
+    else :
+        return 'Method not allowed'
