53df0b228aa23ef0f18505466c028022c38a3324
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-06-06 21:29:01 -0300

    Documentation api language paramenter

--- routers/documentation/documentation_route.py
+++ routers/documentation/documentation_route.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from jinja2.exceptions import TemplateNotFound
 
 docs = Blueprint('docs', __name__, template_folder='templates')
 
 @docs.route('/')
 def index():
-    return render_template('documentation/index.html')
+    language = request.args.get('language')
+    try :
+        return render_template(f'documentation/{language}/index.html')
+    except TemplateNotFound:
+        print(f'Language not found : {language}')
+        return render_template('documentation/pt/index.html')
