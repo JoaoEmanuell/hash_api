0ff02b29f9ed71e5f17b160633bae4511b5438a0
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-06-08 21:18:18 -0300

    Documentation language ts and js

---
+++ static/documentation_languages.js
@@ -0,0 +1,51 @@
+"use strict";
+function main() {
+    const supported_languages = {
+        'Português': 'pt-br',
+        'English': 'en'
+    };
+    const url_treated = window.location.href.split('/');
+    if (url_treated[3] == 'docs') {
+        const div_translated = document.getElementById('translated');
+        // Elements
+        const li_base_element = document.createElement('li');
+        const a_base_element = document.createElement('a');
+        const ul_base_element = document.createElement('ul');
+        // a element atributes
+        a_base_element.setAttribute('class', 'nav-link dropdown-toggle');
+        a_base_element.setAttribute('href', '#');
+        a_base_element.setAttribute('id', 'navbarDropdown');
+        a_base_element.setAttribute('role', 'button');
+        a_base_element.setAttribute('data-bs-toggle', 'dropdown');
+        a_base_element.setAttribute('aria-expanded', 'false');
+        a_base_element.innerHTML = 'Language';
+        li_base_element.className = 'nav-item dropdown';
+        // ul element atributes
+        ul_base_element.setAttribute('class', 'dropdown-menu');
+        ul_base_element.setAttribute('aria-labelledby', 'navbarDropdown');
+        // Dropdown Menu
+        // Object.entries convert a object to array
+        Object.entries(supported_languages).forEach(element => {
+            const li_internal_element = document.createElement('li');
+            const ul_internal_a_element = document.createElement('a');
+            ul_internal_a_element.setAttribute('href', `/docs/?language=${element[1]}`);
+            ul_internal_a_element.setAttribute('class', 'dropdown-item');
+            ul_internal_a_element.innerHTML = element[0];
+            li_internal_element.appendChild(ul_internal_a_element);
+            // Append in ul
+            ul_base_element.appendChild(li_internal_element);
+            // Verify if the language is the last current language
+            if (element[1] != Object.entries(supported_languages)[Object.entries(supported_languages).length - 1 // Last element
+            ][1]) {
+                const hr = document.createElement('hr');
+                hr.setAttribute('class', 'dropdown-divider');
+                ul_base_element.appendChild(hr);
+            }
+        });
+        // Append elements
+        li_base_element.appendChild(a_base_element);
+        li_base_element.appendChild(ul_base_element);
+        div_translated.appendChild(li_base_element);
+    }
+}
+main();
